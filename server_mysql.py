857a3034068e80c9c4ac14ba32214d44e6757d94
FourForward <553630934@qq.com>
2020-05-10 12:06:15 +0800

    添加了django，添加了H5的基础页面

--- server_mysql.py
+++ server_mysql.py
@@ -51,14 +51,38 @@ class Database:
             try:
                 self.cur.execute(eval(f"sql{i}"))
                 self.db.commit()
-                print("ok")
+                print("数据库创建成功")
             except:
                 self.db.rollback()
-                print("no")
+                print("数据库已经存在")
         self.initialize2()
 
     def initialize2(self):
         """
-        初始化第二步，查看最后一场的对局表ID，跳转相应对局表，查看该表内对局数量是否达到阈值，作出相应处理
+        初始化第二步，检查是否存在空的场次表，如果有，就删除
         """
-        pass
+        sql1 = 'select max(s_id) from laodifang_doudizhu.session'
+        sql2 = 'select b_id from laodifang_doudizhu.bout1 where s_id = %s limit 1'
+        sql3 = 'delete from laodifang_doudizhu.session where s_id = %s'
+        self.cur.execute(sql1)
+        number = self.cur.fetchone()
+        if not number:
+            for i in range(1,number+1):
+                self.cur.executemany(sql2,(i,))
+                if  not self.cur.fetchone():
+                    try:
+                        self.cur.executemany(sql3, (i,))
+                        self.db.commit()
+                        print(f'删除空场次 s_id = {i}')
+                    except:
+                        self.db.rollback()
+                        print(f'删除空场次 s_id = {i} 失败,失败')
+        else:
+            print('全新数据库，暂无场次数据')
+
+
+if __name__ == '__main__':
+    abc = Database('laodifang_doudizhu','mu7401889')
+    abc.initialize_databese()
+
+
